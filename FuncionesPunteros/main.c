84f8c95aa12a97aba03b0f5221eb66eb44efbe42
nahuelvallejos21 <nahuelvallejos36@gmail.com>
2018-05-17 10:37:46 -0300

    FuncionesPunteros

---
+++ FuncionesPunteros/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+void intercambiar(int num1,int num2);
+void intercambiarConPunteros(int *num1,int *num2);
+int main()
+{
+    int edadUno;
+    int edadDos;
+    edadUno = 22;
+    edadDos = 99;
+    //intercambiar(edadUno,edadDos);
+    intercambiarConPunteros(&edadUno,&edadDos);
+    printf("\nEdad 1:%d",edadUno);
+    printf("\nEdad 2:%d",edadDos);
+    return 0;
+}
+void intercambiar(int num1,int num2)
+{
+   int aux;
+   aux = num1;
+   num1=num2;
+   num2=aux;
+}
+void intercambiarConPunteros(int *num1,int *num2)
+{
+   int aux;
+   aux = *num1;
+   *num1=*num2;
+   *num2=aux;
+
+}
+
